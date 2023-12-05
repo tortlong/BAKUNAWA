970b9310443e40717c007720b85f5b0296242738
Caleb Josh Berandoy <calebjosh.berandoy@outlook.com>
2023-12-05 23:22:21 +0800

    Added changes: - Added border to game screen, resized player name and score - Added back button to how to play screen

--- projectsample/Functions.cpp
+++ projectsample/Functions.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <conio.h>
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <chrono>
@@ -216,31 +217,38 @@ int gameEngine(sf::RenderWindow& window, const std::string& playerName) {
     }
 
     //Sprites
-    Sprite background;
+    /*Sprite background;
     background.setTexture(desert);
     background.setScale(0.4, 0.4);
-    FloatRect titleBounds = background.getLocalBounds();
+    FloatRect titleBounds = background.getLocalBounds();*/
+    sf::Color brownColor(139, 69, 19);
+    RectangleShape background(Vector2f(800, 600));
+    background.setFillColor(brownColor);
+
+    //border
+    RectangleShape border(Vector2f(700, 500));
+    border.setTexture(&desert);
+    //border.setFillColor(Color::Black);
+    border.setPosition((window.getSize().x - border.getSize().x) / 2, (window.getSize().y - border.getSize().y) / 2);
 
     //Player Name
-    Text playerNameText(playerName, Arial, 24);
-    playerNameText.setPosition(100,20);
+    Text playerNameText(playerName, Arial, 30);
+    playerNameText.setPosition(100,10);
     playerNameText.setStyle(Text::Bold);
 
     //Score
-    Text score("SCORE: " + to_string(sc), Arial, 20);
+    Text score("SCORE: " + to_string(sc), Arial, 30);
     score.setFillColor(Color::White);
     score.setStyle(Text::Bold);
     FloatRect scoreBounds = score.getLocalBounds();
-    score.setPosition(690, 20);
+    score.setPosition(600, 10);
 
     //Render Apple
-    RectangleShape squares(Vector2f(20, 20));
+    RectangleShape squares(Vector2f(30, 30));
     squares.setPosition(apple.x, apple.y);
     squares.setTexture(&appl);
 
-    RectangleShape border(Vector2f(400, 300));
-    border.setFillColor(Color::White);
-    border.setOrigin(400, 300);
+
 
     //Setup SNAKE(wemby)
     init_snake(&wemby.head, &wemby.tail);
@@ -255,14 +263,15 @@ int gameEngine(sf::RenderWindow& window, const std::string& playerName) {
         //Check if eat apple
         RectangleShape haed(Vector2f(20, 20));
         haed.setPosition(wemby.head->x, wemby.head->y);
+
         if (haed.getGlobalBounds().intersects(squares.getGlobalBounds())) {
             munch.stop();
             munch.play();
             grow(&wemby);
 
             do {
-                apple.x = rand() % 800;
-                apple.y = rand() % 600;
+                apple.x = rand() % 700;
+                apple.y = rand() % 500;
             } while (appleError(apple, wemby.head));
 
             sc++;
@@ -283,13 +292,13 @@ int gameEngine(sf::RenderWindow& window, const std::string& playerName) {
         //render
         window.clear();
         window.draw(background);
+        window.draw(border);
         draw_snake(wemby.head, window);
         window.draw(squares);
         window.draw(playerNameText);
         window.draw(score);
-        window.draw(border);
 
-        Sleep(60);
+        Sleep(10);
 
         window.display();
 
@@ -367,17 +376,22 @@ void instructions(sf::RenderWindow& window) {
         return;
     }
 
+    Text back("BACK", pix, 20);
+    back.setFillColor(Color::White);
+    back.setStyle(Text::Bold);
+    back.setPosition(30,20);
+
     Text contr("CONTROLS", pix, 40);
-    contr.setFillColor(Color::White);
+    contr.setFillColor(Color::Yellow);
     contr.setStyle(Text::Bold);
     FloatRect contrBounds = contr.getLocalBounds();
-    contr.setPosition(window.getSize().x / 2 - contrBounds.width / 2, window.getSize().y / 2 -160);
+    contr.setPosition(window.getSize().x / 2 - contrBounds.width / 2, window.getSize().y / 2 -200);
 
     Text howto("HOW TO PLAY", pix, 40);
-    howto.setFillColor(Color::White);
+    howto.setFillColor(Color::Yellow);
     howto.setStyle(Text::Bold);
     FloatRect howtoBounds = howto.getLocalBounds();
-    howto.setPosition(window.getSize().x / 2 - howtoBounds.width / 2, window.getSize().y / 2 - 30);
+    howto.setPosition(window.getSize().x / 2 - howtoBounds.width / 2, window.getSize().y / 2 - 10);
 
     while (window.isOpen()) {
         Event event;
@@ -386,8 +400,15 @@ void instructions(sf::RenderWindow& window) {
                 window.close();
             }
         }
+        if (event.type == Event::MouseButtonPressed) {
+            Vector2i mousePos = Mouse::getPosition(window);
 
+            if (back.getGlobalBounds().contains(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y))) {
+                return;
+            }
+        }
         window.clear();
+        window.draw(back);
         window.draw(contr);
         window.draw(howto);
         window.display();
@@ -399,7 +420,7 @@ std::string entername(sf::RenderWindow& window) {
     using namespace sf;
 
     RectangleShape enterNameBox(Vector2f(500.f, 100.f));
-    enterNameBox.setFillColor(Color::Yellow);
+    enterNameBox.setFillColor(Color::Blue);
     enterNameBox.setPosition((window.getSize().x - enterNameBox.getSize().x) / 2, (window.getSize().y - enterNameBox.getSize().y) / 2);
 
     Font font;
@@ -445,3 +466,16 @@ std::string entername(sf::RenderWindow& window) {
         window.display();
     }
 }
+
+void saveScore(const std::string& playerName, int score) {
+    std::ofstream leaderboardFile("leaderboard.txt", std::ios::app);
+
+    if (leaderboardFile.is_open()) {
+        leaderboardFile << playerName << " " << score << "\n";
+        leaderboardFile.close();
+    }
+    else {
+        return;
+    }
+}
+
